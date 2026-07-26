fa4b2d4f8e984dcbd3e70a903668f17bf95dd7ec
Nawder Loswin <umaywant2@duck.com>
2026-07-25 20:04:51 -0400

    Add exampleSuiteGraph function to process exampleGraph

--- src/examples/example-suite.graph.bundle.js
+++ src/examples/example-suite.graph.bundle.js
@@ -1,1 +1,15 @@
+import { exampleGraph, exampleClarity } from "./example-bundle.js";
+
+export function exampleSuiteGraph() {
+  return {
+    nodes: exampleGraph.nodes.map((node, index) => ({
+      id: node.id,
+      label: node.label ?? `Node ${index + 1}`,
+      payload: node.payload,
+      clarity_surface: exampleClarity.clarity_surface,
+      clarity_scan: exampleClarity.clarity_scan
+    })),
+    edges: exampleGraph.edges
+  };
+}
 
