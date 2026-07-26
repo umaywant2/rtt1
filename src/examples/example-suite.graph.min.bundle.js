f0fe06589d985b3f76eee4ffcf32d47ae15e90df
Nawder Loswin <umaywant2@duck.com>
2026-07-25 20:09:08 -0400

    Update example-suite.graph.min.bundle.js

--- src/examples/example-suite.graph.min.bundle.js
+++ src/examples/example-suite.graph.min.bundle.js
@@ -1,1 +1,2 @@
+import{exampleGraph as g,exampleClarity as c}from"./example-bundle.js";export function exampleSuiteGraph(){return{nodes:g.nodes.map((n,i)=>({id:n.id,label:n.label??`Node ${i+1}`,payload:n.payload,clarity_surface:c.clarity_surface,clarity_scan:c.clarity_scan})),edges:g.edges};}
 
