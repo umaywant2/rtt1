0b410880ad55967e8eabf6fe67583b26f83bcbf6
Nawder Loswin <umaywant2@duck.com>
2026-08-04 01:01:46 -0400

    Update gpu-substrate-primitives.client.js

--- api/gpu-substrate-primitives.client.js
+++ api/gpu-substrate-primitives.client.js
@@ -1,1 +1,9 @@
+export async function gpuSubstratePrimitives(substrate) {
+  const response = await fetch("/api/gpu-substrate-primitives", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(substrate)
+  });
 
+  return response.json();
+}
