6dc6dff5bb7e5b9ff282b932412677172f52624c
Nawder Loswin <umaywant2@duck.com>
2026-08-04 01:02:06 -0400

    Implement message handling in GPU worker

--- api/gpu-substrate-primitives.worker.js
+++ api/gpu-substrate-primitives.worker.js
@@ -1,1 +1,17 @@
+importScripts("/src/gpu/gpu-substrate-primitives.js");
 
+self.onmessage = (event) => {
+  try {
+    const result = GpuSubstratePrimitives.build(event.data.substrate);
+
+    self.postMessage({
+      ok: true,
+      result
+    });
+  } catch (error) {
+    self.postMessage({
+      ok: false,
+      error: error.message || "GS1-001: Invalid substrate input"
+    });
+  }
+};
